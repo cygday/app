01f8480ca6bfcffb353e3ee034b0b87c1eab5ce2
cygday <amethystpun98@gmail.com>
2026-04-18 11:02:30 +0545

    edit /app/lib/supage.tx

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -6,4 +6,4 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
